aa551f8158c30c0edb14e57018bca1d8371856a1
Lisette Spalding <lisette.spalding@tooeleschools.org>
2021-01-20 12:14:58 -0700

    01/20/2021 Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# MADE BY: Lisette Spalding
+# FILE NAME: main.py
+# DATE CREATED: 01/14/2021
+# DATE LAST MODIFIED: 01/14/2021
+
+######### IMPORTS #########
+from tkinter import *
+########### FIN ###########
+
+root = Tk() # Every Tkinter project needs this
+root.title("First GUI!")
+root.geometry("400x400") # Setting the frame (window) height and width
+root.configure(bg = "#f0b14d")
+
+root.mainloop()
